15b603803239931afa85ff7d981a433308c5fec5
Konopl9 <maksimus2998@gmail.com>
2021-07-21 23:11:32 +0200

    End.

--- CSS-The Complete Guide/shared.js
+++ CSS-The Complete Guide/shared.js
@@ -4,13 +4,17 @@ var noButton = document.querySelector('.modal .modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector('.main-nav__item--cta');
 
 for(var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
         // modal.style.display = 'block';
         // backdrop.style.display = 'block';
         modal.classList.add('open');
-        backdrop.classList.add('open');
+        backdrop.style.display = 'block';
+        setTimeout(function() {
+            backdrop.classList.add('open');
+        }, 10);
     });
 }
 if(noButton) {
@@ -31,12 +35,31 @@ function closeModal(){
         modal.classList.remove('open');
     }
     backdrop.classList.remove('open');
+    setTimeout(function() {
+        backdrop.style.display = 'none';
+    }, 200);
 };
 
 toggleButton.addEventListener('click', function() {
     // mobileNav.style.display = 'block';
     // backdrop.style.display = 'block';
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display = 'block';
+    setTimeout(function() {
+        backdrop.classList.add('open');
+    }, 10);
+
+});
+
+ctaButton.addEventListener('animationstart', function(event) {
+
+});
+
+ctaButton.addEventListener('animationend', function(event) {
+
+});
+
+ctaButton.addEventListener('animationiteration', function(event) {
+
 });
 
